ace49437f963f1ba6724f3a76eede6d36f8ab630
Imnyeong <lmh4873@gmail.com>
2023-02-24 20:21:57 +0900

    [230224] - 양측 벽에 콜라이더 추가 - 게임 비율 강제 고정

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    void Start()
+    {
+        Camera camera = this.gameObject.GetComponent<Camera>();
+        Rect rect = camera.rect;
+        float scaleHeight = ((float)Screen.width / Screen.height) / (9.0f / 16.0f);
+        float scaleWidth = 1.0f / scaleHeight;
+        if (scaleHeight < 1)
+        {
+            rect.height = scaleHeight;
+            rect.y = (1.0f - scaleHeight) / 2.0f;
+        }
+        else
+        {
+            rect.width = scaleWidth;
+            rect.x = (1.0f - scaleWidth) / 2.0f;
+        }
+        camera.rect = rect;
+        // 카메라 비율 강제 고정 9 : 16
+    }
+}
