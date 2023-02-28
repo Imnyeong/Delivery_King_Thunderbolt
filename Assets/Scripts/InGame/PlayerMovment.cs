819b9188b786d8a2fd54fa1350ab43b228c89e50
Imnyeong <lmh4873@gmail.com>
2023-02-28 20:57:22 +0900

    [230228] - PC 이동 코드 주석 처리

--- Assets/Scripts/InGame/PlayerMovment.cs
+++ Assets/Scripts/InGame/PlayerMovment.cs
@@ -84,25 +84,25 @@ public class PlayerMovment : MonoBehaviour
         else
             animator.SetInteger(animationType, moveIdle);
 
-        if ((Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) 
-            || (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)))
-            // 왼쪽, 오른쪽 키가 동시에 눌렸거나 둘 다 눌리지 않은 경우
-            animator.SetInteger("MoveType", moveIdle);
-            // 움직임 없이 Idle 애니메이션 실행
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        // 왼쪽 키가 눌린 경우
-        {
-            animator.SetInteger("MoveType", moveLeft);
-            playerRect.anchoredPosition -= new Vector2(speedValue, 0.0f);
-            // Left 애니메이션 실행, 위치 X값 speedValue 만큼 왼쪽으로 이동
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        // 왼쪽 키가 눌린 경우
-        {
-            animator.SetInteger("MoveType", moveRight);
-            playerRect.anchoredPosition += new Vector2(speedValue, 0.0f);
-            // Right 애니메이션 실행, 위치 X값 speedValue 만큼 오른쪽으로 이동
-        }
+        //if ((Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) 
+        //    || (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)))
+        //    // 왼쪽, 오른쪽 키가 동시에 눌렸거나 둘 다 눌리지 않은 경우
+        //    animator.SetInteger("MoveType", moveIdle);
+        //    // 움직임 없이 Idle 애니메이션 실행
+        //else if (Input.GetKey(KeyCode.LeftArrow))
+        //// 왼쪽 키가 눌린 경우
+        //{
+        //    animator.SetInteger("MoveType", moveLeft);
+        //    playerRect.anchoredPosition -= new Vector2(speedValue, 0.0f);
+        //    // Left 애니메이션 실행, 위치 X값 speedValue 만큼 왼쪽으로 이동
+        //}
+        //else if (Input.GetKey(KeyCode.RightArrow))
+        //// 왼쪽 키가 눌린 경우
+        //{
+        //    animator.SetInteger("MoveType", moveRight);
+        //    playerRect.anchoredPosition += new Vector2(speedValue, 0.0f);
+        //    // Right 애니메이션 실행, 위치 X값 speedValue 만큼 오른쪽으로 이동
+        //}
     }
     IEnumerator OilCoroutine()
     {
