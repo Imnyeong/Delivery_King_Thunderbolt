e3b3cb5d75db8fe86eb65e705966503e2391089f
Imnyeong <lmh4873@gmail.com>
2023-03-02 22:54:32 +0900

    [230302] - Start Sound 추가 - Button Sound 교체

--- Assets/Scripts/Intro/IntroManager.cs
+++ Assets/Scripts/Intro/IntroManager.cs
@@ -5,10 +5,12 @@ public class IntroManager : MonoBehaviour
 {
     string inGame = "InGame";
     int buttonSound = 0;
+    int playSound = 3;
     void Start() => GameManager.Instance.BGMPlay();
     public void OnClickStart() => SceneManager.LoadScene(inGame);
     // 시작 버튼 =>  InGame Scene으로 이동
     public void OnClickExit() { Application.Quit(); }
     // 종료 버튼 => Application 종료
     public void PlayButtonSound() => GameManager.Instance.EffectPlay(buttonSound);
+    public void PlayStartSound() => GameManager.Instance.EffectPlay(playSound);
 }
